b44e235a4934d4626b87289e9f43149d7d98b9c0
ChenQuanwei <chen_quanwei@qq.com>
2023-03-04 21:43:10 +0800

    init

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,16 +6,16 @@ const int mod = 1e9 + 7;
 const int INF = 0x3f3f3f3f;
 const int maxn = 1e6 + 10;
 
-
-
 int main()
 {
-    #ifdef cqw
-    freopen("F:/ACM/data/in.txt", "r", stdin);
-    freopen("F:/ACM/data/out.txt", "w", stdout);
-    #endif
+#ifdef cqw
+    freopen("./data/in.txt", "r", stdin);
+    freopen("./data/out.txt", "w", stdout);
+#endif
 
-    
+    cout<<"hello world"<<endl;
+
+
 
     return 0;
 }
