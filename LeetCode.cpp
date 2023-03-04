6f6d279f0c3c32faf32a10cce918ddb9a97fd188
ChenQuanwei <chen_quanwei@qq.com>
2023-03-04 19:39:55 +0800

    init

---
+++ LeetCode.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const doubleEPS = 1e-8;
+const int mod = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const int maxn= 1e6 + 10;
+
+
+
+int main()
+{
+ #ifdef cqw
+freopen("/Users/bytedance/cqw/acm/data/in.txt", "r", stdin);
+ //freopen("/Users/bytedance/cqw/acm/data/out.txt", "w", stdout);
+ #endif
+
+
+
+ return 0;
+}
