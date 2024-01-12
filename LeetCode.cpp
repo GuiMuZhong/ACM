d5120dbe6ff7ff88280d31e202df8356e2262f49
ChenQuanwei <chen_quanwei@qq.com>
2024-01-12 18:38:29 +0800

    test push

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,18 +6,4 @@ const int mod = 1e9 + 7;
 const int INF = 0x3f3f3f3f;
 const int maxn = 1e6 + 10;
 
-// int using 
 
-int main()
-{
-#ifdef cqw
-    freopen("./data/in.txt", "r", stdin);
-    freopen("./data/out.txt", "w", stdout);
-#endif
-
-    cout<<"hello world"<<endl;
-
-
-
-    return 0;
-}
